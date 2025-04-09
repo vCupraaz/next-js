88fb75de33165f6b64c88beb0ee8fc5fbf54c850
vCupraaz <echaniz.valentin@gmail.com>
2025-04-09 15:09:04 +0200

    Mise à jour chapitre 12

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,6 +1,7 @@
 'use server';
 
 import { z } from 'zod';
+import postgres from 'postgres';
 
 const FormSchema = z.object({
     id: z.string(),
@@ -10,8 +11,21 @@ const FormSchema = z.object({
     date: z.string(),
 });
 
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
 export async function createInvoice(formData: FormData) {
-    // ...
+    const { customerId, amount, status } = CreateInvoice.parse({
+        customerId: formData.get('customerId'),
+        amount: formData.get('amount'),
+        status: formData.get('status'),
+    });
+    const amountInCents = amount * 100;
+    const date = new Date().toISOString().split('T')[0];
+
+    await sql`
+    INSERT INTO invoices (customer_id, amount, status, date)
+    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+  `;
 }
